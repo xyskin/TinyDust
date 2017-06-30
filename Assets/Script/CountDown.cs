26e7abd3e007e6ef55b9408009ffeb0232ed3984
xyskin <xyskin@163.com>
2017-06-30 14:36:58 +0800

    Neko 14:36

---
+++ Assets/Script/CountDown.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountDown : MonoBehaviour {
+	float fps=0;
+	float startTime=0;
+	public float deltaT;
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		startTime = Time.realtimeSinceStartup;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fps = 1 / Time.deltaTime;
+		deltaT = Time.realtimeSinceStartup - startTime;
+		//Debug.Log (deltaT.ToString ("f2"));
+
+		if (10 - deltaT < 5 && 10 - deltaT > 0) {
+			text.color = Color.red;
+			text.text = (10 - deltaT).ToString ("f1");
+		} else if (10 - deltaT > 5) {
+			text.color = Color.black;
+			text.text = (10 - deltaT).ToString ("f1");
+		} else {
+			text.text = "";
+			text.color = Color.black;
+		}
+	}
+}
--- Assets/Script/ThirdPersonUserControl1.cs
+++ Assets/Script/ThirdPersonUserControl1.cs
@@ -5,44 +5,49 @@ using UnityStandardAssets.CrossPlatformInput;
 namespace UnityStandardAssets.Characters.ThirdPerson
 {
    
-    public class ThirdPersonUserControl1 : MonoBehaviour
-    {
-        private ThirdPersonCharacter m_Character; // A reference to the ThirdPersonCharacter on the object
-        private Transform m_Cam;                  // A reference to the main camera in the scenes transform
-        private Vector3 m_CamForward;             // The current forward direction of the camera
-        private Vector3 m_Move;
-        private bool m_Jump;                      // the world-relative desired move direction, calculated from the camForward and user input.
+	public class ThirdPersonUserControl1 : MonoBehaviour
+	{
+		private ThirdPersonCharacter m_Character;
+		// A reference to the ThirdPersonCharacter on the object
+		private Transform m_Cam;
+		// A reference to the main camera in the scenes transform
+		private Vector3 m_CamForward;
+		// The current forward direction of the camera
+		private Vector3 m_Move;
+		private bool m_Jump;
+		// the world-relative desired move direction, calculated from the camForward and user input.
 
         
-        private void Start()
-        {
+		private void Start ()
+		{
                   
-        }
+		}
 
 
-        private void Update()
-        {
-            if (!m_Jump)
-            {
-                m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-            }
-        }
+		private void Update ()
+		{
+			if (!m_Jump) {
+				m_Jump = CrossPlatformInputManager.GetButtonDown ("Jump");
+			}
+		}
 
 
-        // Fixed update is called in sync with physics
-        private void FixedUpdate()
-        {
-            // read inputs
-            float h = CrossPlatformInputManager.GetAxis("Horizontal1");
-            float v = CrossPlatformInputManager.GetAxis("Vertical1");
+		// Fixed update is called in sync with physics
+		private void FixedUpdate ()
+		{
+			// read inputs
+
+
+			float h = CrossPlatformInputManager.GetAxis ("Horizontal1");
+			float v = CrossPlatformInputManager.GetAxis ("Vertical1");
         
-			m_Move = v*Vector3.forward*100 + h*Vector3.right*100;
+			m_Move = v * Vector3.forward * 100 + h * Vector3.right * 100;
 
-            // pass all parameters to the character control script
-			this.transform.Translate(m_Move,Space.World);
-            m_Jump = false;
-        }
+			// pass all parameters to the character control script
+			this.transform.Translate (m_Move, Space.World);
+			m_Jump = false;
+		}
 
 
-    }
+	}
 }
